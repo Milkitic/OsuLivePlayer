5a7c92896d42fcbd9185413ea1279a0c25953e14
Milkitic <milkitic@126.com>
2019-08-16 23:15:12 +0800

    move files

---
+++ OsuLivePlayer.WPF/Sync/OsuLivePlayerPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OsuRTDataProvider;
+using Sync.Plugins;
+
+namespace OsuLivePlayer.WPF.Sync
+{
+    [SyncRequirePlugin(typeof(OsuRTDataProviderPlugin))]
+    class OsuLivePlayerPlugin : Plugin
+    {
+        public OsuLivePlayerPlugin() : base("OsuLivePlayerPlugin.WPF", "yf_bmp")
+        {
+        }
+    }
+}
